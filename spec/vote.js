2674e96137b4b80bf22222e2b23b1746ea796a4f
shreya390 <67576673+shreya390@users.noreply.github.com>
2021-07-20 11:04:31 +0530

    Update vote.js

--- spec/vote.js
+++ spec/vote.js
@@ -1,28 +1,24 @@
 
-		let models={
+		const prompt = require('prompt-sync')();
+		var models={
 			0:0,
 			1:0,
 			2:0,
 			3:0
 		  }
-		   let model_values="";
-		  let x="";
+		  var  model_values="";
+		  var option="";
 		  
-		  function registerNewVote(){
+		  function inputValue(){
+		   option=prompt("Which is the best indian car?\n0: Maruthi\n1: Hyundai\n2: KIA\n3: Ford");
+		  return option;
 		  }
 		  
-		   function inputValue(){
-			x=(prompt("Which is the best indian car?\n0: Maruthi\n1: Hyundai\n2: KIA\n3: Ford"));
-		   return x;
-		   }
-		  
-		  function evaluateVotes(x){
-			let option =x;
-		  
+		  function evaluateVotes(option){
 			try{
-			  if(option<0||option>3){x=String(x); throw "enter valid number"}
-			  if (isNaN(option)){x=String(x); throw "enter only number"}
-		  
+			  if(option<0||option>3) throw "enter valid number"
+			  if (isNaN(option)){option=String(option); throw "enter only number"}
+			  
 			}
 			catch(err){
 		  
@@ -34,17 +30,21 @@
 		   value++;
 		   models[option]=value;
 		   model_values= Object.values(models);
-		  
+		   
 		   return displayResults(model_values);
 		  }
 		  }
-		  
-		  
-		  
 		  function displayResults(model_values){
-		  
+			 
 		   return  "Poll results are "+model_values;
 		  }
+		  function registerNewVote(){
+			var option =inputValue();
+		   var result= evaluateVotes(option);
+		   return result;
+		   
+		  }
+		  console.log(registerNewVote());
 		  module.exports={
 			evaluateVotes
 		  }
